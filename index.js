92480dbf37019af1b725b22fd67e1e0dc1556ddf
Konstantin Kitmanov <doctor.hogart@gmail.com>
2015-11-23 16:28:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const request = require('request');
+const isRegex = require('is-regex');
+
+/**
+ *
+ * @param {Object} app express instance
+ * @param {Array.<Array>} config array of 2-elements arrays
+ */
+function reproxy (app, config) {
+    if (!Array.isArray(config)) {
+        throw new TypeError('Config should be array');
+    }
+
+    config.forEach((configLine) => {
+        const listen = configLine[0];
+        const redirect = configLine[1];
+
+        app.all(listen, (req, res) => {
+            const url = isRegex(listen) ? req.originalUrl.replace(listen, redirect) : redirect + listen;
+            const requestOptions = {
+                url,
+                method: req.method,
+                qs: req.query,
+                body: req.body
+            };
+
+            req.pipe(request(requestOptions)).pipe(res);
+        })
+    });
+}
---
+++ readme.md
@@ -0,0 +1,25 @@
+reproxy
+=======
+
+Simple utility, intended for proxying requests to remote services if CORS is not available for some reason. Usually this
+is useful for development environments.
+
+Install with `npm install hogart/reproxy -s`. Requires recent nodejs version (4.x should work).
+
+## Usage
+
+```js
+const app = express();
+require('reproxy')(app, [[/\someEndpoint\/(.*)/, 'http://real-endpoint/api/$1']]);
+```
+
+## Config
+
+Config looks like following:
+```js
+[
+    [/\someEndpoint\/(.*)/, 'http://real-endpoint/api/$1'], // regexp and replacement string
+    [/\otherEndpoint\/(.*)/, (match, group1) => { return 'http://other-endpoint/' + group1.split('/').reverse().join('/'); }], // regexp and replacement function
+    ['/simpleString/', 'http://example.com'] // request will be made to `'http://example.com' + '/simpleString/'`;
+]
+```
